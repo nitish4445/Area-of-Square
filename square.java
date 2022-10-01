a16176d0d7209ec3e6bfa712b55ee9a3ed016e8e
nitish4445 <nitishkumar09598@gmail.com>
2022-10-01 21:19:27 +0530

    first commit

---
+++ square.java
@@ -0,0 +1,16 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class square{
+    public static void main (String[] args) {
+                      // Yoint a= sc.nextInt();
+    System.out.print("enter a number ");
+    Scanner sc = new Scanner(System.in);
+    int a= sc.nextInt();
+  
+    System.out.print(a*a);
+    
+    }
+}
